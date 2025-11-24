a90921c00874b34ccfaab32140b1570907e085ba
Marcellino Abdelmalak <me@excalian.com>
2025-11-24 11:13:17 -0800

    Added walls and made progressive difficulty with more enemies spawning

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,8 +3,15 @@ extends Node
 var spawn_radius: float
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+
+@onready var timer = $Timer
+
+var base_spawn_time: float
+
 
 func _ready() -> void:
+	base_spawn_time = timer.wait_time
 	var viewport_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 	var viewport_height = ProjectSettings.get_setting("display/window/size/viewport_height")
 	
@@ -14,10 +21,14 @@ func _ready() -> void:
 		spawn_radius = viewport_height
 	spawn_radius += 40
 	
-	$Timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
+	
+	timer.timeout.connect(on_timer_timeout)
 
 
 func on_timer_timeout():
+	timer.start()
+	
 	var player: Node2D = get_tree().get_first_node_in_group("player")
 	if player == null:
 		return
@@ -33,3 +44,9 @@ func on_timer_timeout():
 	
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	var time_off = (.1 / 12) * arena_difficulty
+	time_off = min(time_off, .7)
+	timer.wait_time = base_spawn_time - time_off
