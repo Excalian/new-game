096f2238b7dc7671d3b8a011babe89838d7ff441
Marcellino Abdelmalak <me@excalian.com>
2025-11-24 00:15:05 -0800

    Added an end screen that includes a victory and defeat screen, made it so that you can lose or win

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,7 +1,18 @@
 extends Node
 
+@export var end_screen_scene: PackedScene
+
 @onready var timer = $Timer # assigning a node to a variable when the node is ready
+
+
+func _ready() -> void:
+	timer.timeout.connect(on_timer_timeout)
 
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func on_timer_timeout():
+	var end_screen_instance = end_screen_scene.instantiate()
+	add_child(end_screen_instance)
