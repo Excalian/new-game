65cc2f7854c0b1899c5d2d6ad5dfc1cc70b89af1
Marcellino Abdelmalak <me@excalian.com>
2025-11-24 17:00:00 -0800

    Added a wizard enemy

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-@onready var visuals: Node2D = $Visuals
 @onready var velocity_component: Node = $VelocityComponent
+@onready var visuals: Node2D = $Visuals
 
 
 func _process(_delta: float) -> void:
@@ -10,4 +10,5 @@ func _process(_delta: float) -> void:
 	
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
-		visuals.scale = Vector2(-move_sign, 1)
+		visuals.scale = Vector2(move_sign, 1)
+	
